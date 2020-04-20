2a1abc2001fb62f30256e29761cc5c26ec3a0ee9
joaquingabad <joaquingabad@gmail.com>
2020-04-20 14:47:52 +0200

    Primer commit para crear repo

--- Hola mundo.R
+++ Hola mundo.R
@@ -1,7 +1,9 @@
 print("Hola mundo")
 #Vamos crear el proyecto
 usethis::use_github("C:/Users/demer/OneDrive/Documentos/prueba")
-2
 usethis::browse_github_pat()
 usethis::edit_r_environ()
 githubpat<-"f141d9b2dc536323b03b699c32133f35d027c1eb"
+usethis::git_sitrep()
+library(devtools)
+devtools::install_github('joaquingabad/prueba')
