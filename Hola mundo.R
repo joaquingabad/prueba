7fc87935bb9c1b4f7345b6128b767de443a81da8
joaquingabad <joaquingabad@gmail.com>
2020-04-20 14:14:15 +0200

    ff

--- Hola mundo.R
+++ Hola mundo.R
@@ -1,2 +1,7 @@
 print("Hola mundo")
 #Vamos crear el proyecto
+usethis::use_github("C:/Users/demer/OneDrive/Documentos/prueba")
+2
+usethis::browse_github_pat()
+usethis::edit_r_environ()
+githubpat<-"f141d9b2dc536323b03b699c32133f35d027c1eb"
