b6c582467eeb529370c3c584c4d6ae8664ca6272
joaquingabad <joaquingabad@gmail.com>
2020-04-20 14:49:48 +0200

    Corregimos codigo

--- Hola mundo.R
+++ Hola mundo.R
@@ -1,9 +1,2 @@
 print("Hola mundo")
-#Vamos crear el proyecto
-usethis::use_github("C:/Users/demer/OneDrive/Documentos/prueba")
-usethis::browse_github_pat()
-usethis::edit_r_environ()
-githubpat<-"f141d9b2dc536323b03b699c32133f35d027c1eb"
-usethis::git_sitrep()
-library(devtools)
-devtools::install_github('joaquingabad/prueba')
+
